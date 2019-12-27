f60bb63989ea8d0e849f4fa9dded0688e85247e8
Subham Singh <s.chauhan5505@gmail.com>
2019-12-27 13:05:25 +0530

    Add capture screenshot with screenshot_grabber

--- Gauge.CSharp.Lib/GaugeScreenshots.cs
+++ Gauge.CSharp.Lib/GaugeScreenshots.cs
@@ -15,7 +15,9 @@
 // You should have received a copy of the GNU General Public License
 // along with Gauge-CSharp.  If not, see <http://www.gnu.org/licenses/>.
 
+using System;
 using System.Collections.Generic;
+using System.IO;
 
 namespace Gauge.CSharp.Lib
 {
@@ -24,6 +26,13 @@ namespace Gauge.CSharp.Lib
         private static ICustomScreenshotWriter screenshotWriter = new DefaultScreenshotWriter();
 
         internal static List<string> ScreenshotFiles = new List<string>();
+        private static ICustomScreenshotGrabber screenshotGrabber = null;
+
+        public static void RegisterCustomScreenshotGrabber(ICustomScreenshotGrabber customScreenshotGrabber)
+        {
+            screenshotGrabber = customScreenshotGrabber;
+        }
+
 
         public static void RegisterCustomScreenshotWriter(ICustomScreenshotWriter customScreenshotWriter)
         {
@@ -32,7 +41,15 @@ namespace Gauge.CSharp.Lib
 
         public static void Capture()
         {
-            ScreenshotFiles.Add(screenshotWriter.TakeScreenShot());
+            if (screenshotGrabber != null)
+            {
+                var screenshotPath = Path.Combine(Environment.GetEnvironmentVariable("screenshots_dir"), String.Format("screenshot-{0}.png", Guid.NewGuid().ToString()));
+                var screenshotBytes = screenshotGrabber.TakeScreenShot();
+                File.WriteAllBytes(screenshotPath, screenshotBytes);
+                ScreenshotFiles.Add(Path.GetFileName(screenshotPath));
+            } else {
+                ScreenshotFiles.Add(screenshotWriter.TakeScreenShot());
+            }
         }
     }
 }
