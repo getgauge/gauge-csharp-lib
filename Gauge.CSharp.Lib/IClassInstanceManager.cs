87fb5ec9b7282b81aebcbb985ee66631c8e8a67f
sriv <srikanth.ddit@gmail.com>
2020-05-19 09:30:00 +0530

    change license to Apache2.0, fixes #9
    Signed-off-by: sriv <srikanth.ddit@gmail.com>

--- Gauge.CSharp.Lib/IClassInstanceManager.cs
+++ Gauge.CSharp.Lib/IClassInstanceManager.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿/*----------------------------------------------------------------
+ *  Copyright (c) ThoughtWorks, Inc.
+ *  Licensed under the Apache License, Version 2.0
+ *  See LICENSE.txt in the project root for license information.
+ *----------------------------------------------------------------*/
+using System;
 using System.Collections.Generic;
 using System.Reflection;
 
