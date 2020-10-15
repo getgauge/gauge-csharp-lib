cb77d3ffb92ccdfde446a18afbba84dc585ffd64
Karim.nabli <karim.nabli.ie@gmail.com>
2020-10-15 11:00:13 +0100

    Remove commented code
    Signed-off-by: Karim.nabli <karim.nabli.ie@gmail.com>

--- Gauge.CSharp.Lib/ScenarioDataStore.cs
+++ Gauge.CSharp.Lib/ScenarioDataStore.cs
@@ -11,40 +11,21 @@ namespace Gauge.CSharp.Lib
         public static object Get(string key)
         {
             return CommonDataStore.WithDataStore(store).Get(key);
-            //lock (store)
-            //{
-            //    object outVal;
-            //    var valueExists = store.Value.TryGetValue(key, out outVal);
-            //    return valueExists ? outVal : null;
-            //}
         }
 
         public static T Get<T>(string key)
         {
             return CommonDataStore.WithDataStore(store).Get<T>(key);
-            //lock (store)
-            //{
-            //    return (T)Get(key);
-            //}
         }
 
         public static void Add(string key, object value)
         {
             CommonDataStore.WithDataStore(store).Add(key, value);
-            //lock (store)
-            //{
-            //    store.Value[key] = value;
-
-            //}
         }
 
         public static void Clear()
         {
             CommonDataStore.WithDataStore(store).Clear();
-            //lock (store)
-            //{
-            //    store.Value.Clear();
-            //}
         }
     }
 }
