f1faaaff3b57490dc427a6dfefa337fd4e17e120
Piotr Nestorow <piotr.nestorow@systemverification.com>
2024-03-27 14:26:18 +0100

    Handle skip scenario exception message
    Signed-off-by: Piotr Nestorow <piotr.nestorow@systemverification.com>

--- Gauge.CSharp.Lib/Attribute/Exceptions.cs
+++ Gauge.CSharp.Lib/Attribute/Exceptions.cs
@@ -12,9 +12,13 @@ namespace Gauge.CSharp.Lib.Attribute
     {
         public SkipScenarioException() : base() { }
 
-        public SkipScenarioException(string message) : base(message) { }
+        public SkipScenarioException(string message) : base(message) { 
+            GaugeMessages.WriteMessage(message);
+        }
 
-        public SkipScenarioException(string message, Exception innerException) : base(message, innerException) { }
+        public SkipScenarioException(string message, Exception innerException) : base(message, innerException) { 
+            GaugeMessages.WriteMessage(message);
+        }
 
         protected SkipScenarioException(System.Runtime.Serialization.SerializationInfo info, System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
     }
